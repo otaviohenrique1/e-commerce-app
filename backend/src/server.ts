62bbc4ebdf406281843e619a7bbab9c41ec46b2c
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-06-08 14:04:36 -0300

    Commit 4 dos arquivos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import errorHandler from "./errors/handler";
 
 const app = express();
 
 app.use(cors);
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(3333, () => console.log("Express iniciado..."))
 

e1a6d18828fb05977d559c60c6dc8e500f148b40
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-06-23 23:11:24 -0300

    Commit 27 dos arquivos

--- mobile_flutter/lib/shared/botao/botao_widget.dart
+++ mobile_flutter/lib/shared/botao/botao_widget.dart
@@ -3,20 +3,31 @@ import 'package:flutter/material.dart';
 class Botao extends StatelessWidget {
   final String label;
   final Function() onCallback;
+  final ButtonStyle? style;
 
   Botao({
     Key? key,
     required this.label,
     required this.onCallback,
+    this.style,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: ElevatedButton(
+        style: style,
+        // style: ElevatedButton.styleFrom(style),
         child: Text(label),
         onPressed: onCallback,
       ),
     );
   }
 }
+
+/*
+Exemplo (ElevatedButton com style):
+  ElevatedButton(
+    style: ElevatedButton.styleFrom(primary: Colors.green),
+  )
+*/
--- mobile_flutter/lib/shared/campo/campo_widget.dart
+++ mobile_flutter/lib/shared/campo/campo_widget.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 
 class Campo extends StatelessWidget {
+  final void Function(String)? onChanged;
+  final TextInputType? keyboardType;
+  final String? initialValue;
+  final TextStyle? style;
+  final String? Function(String?)? validator;
+  final void Function(String?)? onSaved;
+
   Campo({
     Key? key,
+    this.onSaved,
+    this.validator,
+    this.initialValue,
+    this.style,
+    this.keyboardType,
+    this.onChanged,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TextFormField(),
+      child: TextFormField(
+        onChanged: onChanged,
+        keyboardType: keyboardType,
+        style: style,
+        initialValue: initialValue,
+        validator: validator,
+        onSaved: onSaved,
+      ),
     );
   }
 }
